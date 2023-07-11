fe981eb0bd73d38a86382bb9670ae62fd1c066e5
Munesh Chowdary <32429098+muneshchowdary@users.noreply.github.com>
2023-07-11 12:51:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,3 @@
 provider "aws" {
   region = "us-west-2"
 }
-
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "172.16.0.0/16"
-
-  tags = {
-    Name = "tf-example"
-  }
-}
-
-resource "aws_subnet" "my_subnet" {
-  vpc_id            = "${aws_vpc.my_vpc.id}"
-  cidr_block        = "10.0.0.0/8"
-  availability_zone = "us-west-2a"
-
-  tags = {
-    Name = "tf-example"
-  }
-}
