9222cc2441050ae55d719d0924bb1489ea5fdcc5
Munesh Chowdary <32429098+muneshchowdary@users.noreply.github.com>
2023-07-11 14:29:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,43 +1,83 @@
-#terraform {
-  #required_providers {
-    #ubuntu = {
-    #  source  = "hashicorp/aws"
-   #   version = ">= 1.0"
-  #  }
- # }
-#}
-
-terraform {
-  required_version = ">=0.13.0"
+variable "awsprops" {
+    type = "map"
+    default = {
+    region = "us-east-1"
+    vpc = "vpc-5234832d"
+    ami = "ami-0c1bea58988a989155"
+    itype = "t2.micro"
+    subnet = "subnet-81896c8e"
+    publicip = true
+    keyname = "myseckey"
+    secgroupname = "IAC-Sec-Group"
+  }
 }
 
 provider "aws" {
-  region = "us-east-1"
-  access_key = "AKIA4FJAY3AIMRUM345F"
-  secret_key = "Munesh@143"
+  region = lookup(var.awsprops, "region")
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
+resource "aws_security_group" "project-iac-sg" {
+  name = lookup(var.awsprops, "secgroupname")
+  description = lookup(var.awsprops, "secgroupname")
+  vpc_id = lookup(var.awsprops, "vpc")
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  // To Allow SSH Transport
+  ingress {
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+  // To Allow Port 80 Transport
+  ingress {
+    from_port = 80
+    protocol = ""
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  owners = ["835976747024"] # Canonical
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.micro"
 
+resource "aws_instance" "project-iac" {
+  ami = lookup(var.awsprops, "ami")
+  instance_type = lookup(var.awsprops, "itype")
+  subnet_id = lookup(var.awsprops, "subnet") #FFXsubnet2
+  associate_public_ip_address = lookup(var.awsprops, "publicip")
+  key_name = lookup(var.awsprops, "keyname")
+
+
+  vpc_security_group_ids = [
+    aws_security_group.project-iac-sg.id
+  ]
+  root_block_device {
+    delete_on_termination = true
+    iops = 150
+    volume_size = 50
+    volume_type = "gp2"
+  }
   tags = {
-    Name = "HelloWorld"
+    Name ="SERVER01"
+    Environment = "DEV"
+    OS = "UBUNTU"
+    Managed = "IAC"
   }
+
+  depends_on = [ aws_security_group.project-iac-sg ]
+}
+
+
+output "ec2instance" {
+  value = aws_instance.project-iac.public_ip
 }
